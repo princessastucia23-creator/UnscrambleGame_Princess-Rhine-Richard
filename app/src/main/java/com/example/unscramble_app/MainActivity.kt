c7148ef8823849e4d44ce2b928c4ec747ff8a492
LUCKY 88 <apple@Apples-iMac-6.local>
2026-08-19 00:36:51 -0400

    Phase 6 update

---
+++ app/src/main/java/com/example/unscramble_app/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.unscramble_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import com.example.unscramble_app.ui.theme.UnscrambleAppTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            UnscrambleAppTheme {
+                GameScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun GameScreen() {
+    var userAnswer by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "UNSCRAMBLE",
+            fontSize = 30.sp
+        )
+
+        Text(
+            text = "TAC",
+            fontSize = 40.sp
+        )
+
+        Text(
+            text = "Unscramble the word!"
+        )
+
+        OutlinedTextField(
+            value = userAnswer,
+            onValueChange = {
+                userAnswer = it
+            },
+            label = {
+                Text("Enter your answer")
+            }
+        )
+
+        Button(
+            onClick = { }
+        ) {
+            Text("SUBMIT")
+        }
+
+        Text(
+            text = "Score: 0"
+        )
+    }
+}
